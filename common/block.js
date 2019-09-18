197cf8cb690d4fdf1c0e7d41f4343ffcc4eaed20
choi ju young <yesutis1@naver.com>
2019-09-18 12:18:46 +0900

    생성자 함수 생성

--- common/block.js
+++ common/block.js
@@ -1,8 +1,37 @@
 //-------- 문서가 준비(html, 이미지, 스크립트, 스타일 등)되면 실행 -------
 window.onload = function(){
-    this.console.log('기본준비끝')
+    this.console.log(myGameArea.message.docIsReady)
+    myGameArea.start();  // window.myGameArea.start(); ->  윈도우 생략해도 됨
+    this.myGamePiece = new component(30,30,"red",10,120);
 }
 
 //------ 환경 경로를 가진 객체
 var myGameArea = {
+    canvas : document.createElement("canvas"),
+    message : {
+        docIsReady : "문서가 준비되었습니다."
+    },
+    start : function(){
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        // document.body.insertBefore(무엇, 어디)
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+        //2d사용을 위해 context 설정해주기
+        // var ctx = myGameArea.canvas.getContext("2d");
+        this.context = this.canvas.getContext("2d");
+    }
 }
+
+//컴포넌트 생성자 함수
+function component(w,h,c,x,y){
+    this.x = x;
+    this.y = y;
+    this.c = c;
+    this.w = w;
+    this.h = h;
+    ctx = myGameArea.context;
+    ctx.fillStyle = c;
+    ctx.fillRect(this.x, this.y , this.w, this.h);
+}
+
